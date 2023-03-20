5cc972f3f02a45e913c75a597b9ffbf1f87c885b
Sulaiman Hussain <118259411+SulaimanROC@users.noreply.github.com>
2023-03-20 11:26:16 +0100

    Add files via upload

---
+++ sd22-id-p03-vacationpicker-SulaimanROC/README.md
@@ -0,0 +1,9 @@
+# if-sd22-ID-VacationPicker
+
+Beste student, 
+
+Voeg aan deze repository jouw VacationPicker toe. 
+Tijdens de lessen van het keuzedeel Interaction Design ga je steeds een stapje verder met deze applicatie en leer je Vue.js
+Hoe leuk is het om de VacationPicker als extra project uiteindelijk toe te voegen aan je porfolio!
+
+Succes!!
---
+++ sd22-id-p03-vacationpicker-SulaimanROC/src/components/VacationPicker.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="vac">
+    <h1>VacationPicker</h1>
+    <p>Aantal = {{count}}</p>
+    <button class="btn btn-success" v-on:click="count++" >Plus</button>
+    <button class="btn btn-danger" @click="count--"> Min </button>
+
+    <h2>Lijst</h2>
+    <ul class="list-group">
+        <li class="list-group-item" v-for="(number, index) in numberList" v-bind:key="index"> 
+            {{number}}
+        </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VacationPicker',
+  data(){
+    return{
+        count:0,
+        numberList: [1,2,3,4]
+    }
+  }
+        
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
